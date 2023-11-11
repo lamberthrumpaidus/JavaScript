b79ab9a57a062848c90efe67a85da5671c67288a
lam <149516112+LamberthPaulinusRumpaidus@users.noreply.github.com>
2023-11-12 00:50:43 +0700

    Update Palindrom.js

--- Palindrom.js
+++ Palindrom.js
@@ -1,28 +1,19 @@
-function isPalindrome(text) {
-    // Ubah semua huruf menjadi huruf kecil
-    text = text.toLowerCase();
-  
+    function isPalindrome(text) {
+    text = text.toLowerCase().replace(/[^a-zA-Z]/g, ''); // Ubah ke huruf kecil dan hapus karakter non-alfabet
+
     for (let i = 0; i < Math.floor(text.length / 2); i++) {
-      if (text[i] !== text[text.length - i - 1]) {
-        return false;
-      }
+        if (text[i] !== text[text.length - i - 1]) {
+            return false;
+        }
     }
     return true;
-  }
-  
-  const readline = require('readline');
-  
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  
-  rl.question('Masukkan kata: ', (kata) => {
-    if (isPalindrome(kata)) {
-      console.log(`${kata} ini adalah palindrome`);
-    } else {
-      console.log(`${kata} bukan palindrome`);
-    }
-    rl.close();
-  });
-  
+}
+
+// Contoh penggunaan di lingkungan konsol
+const kata = "A man, a plan, a canal, Panama"; // Ganti dengan kata yang ingin Anda periksa
+
+if (isPalindrome(kata)) {
+    console.log(`${kata} ini adalah palindrome`);
+} else {
+    console.log(`${kata} bukan palindrome`);
+}
